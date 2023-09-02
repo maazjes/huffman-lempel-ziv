bc9cf15919b26cb98c15975c74efde6839d780bf
maaz <marzusness@gmail.com>
2023-09-02 16:00:17 +0300

    Fix interface
    Exit the program when user cancels file selection. Loop input if it is
    not accepted.

--- src/main.py
+++ src/main.py
@@ -9,22 +9,25 @@ root.withdraw()
 
 action = ""
 algo = ""
-
+file_path = ""
 
 while True:
     action = input("Would you like to compress or decompress (c/d)? ")
 
-    if action != "c" and action != "d":
-        continue
+    if action == "c" or action == "d":
+        break
 
+while True:
     algo = input("Would you like to use Huffman or LZW (h/l)? ")
 
-    if algo != "h" and algo != "l":
-        continue
-
-    break
+    if algo == "h" or algo == "l":
+        break
 
 file_path = filedialog.askopenfilename()
+
+if not file_path:
+    exit()
+
 file_path_raw = file_path
 
 if file_path.endswith(".txt") or file_path.endswith(".bin"):
@@ -36,13 +39,13 @@ if action == "c":
     if os.path.exists(f"{file_path_raw}.bin"):
         i += 1
 
-    while os.path.exists(f"{file_path_raw}.bin ({i})"):
+    while os.path.exists(f"{file_path_raw} ({i}).bin"):
         i += 1
 
     target = ""
 
     if i != 0:
-        target = f"{file_path_raw}.bin ({i})"
+        target = f"{file_path_raw} ({i}).bin"
 
     if algo == "h":
         huffman.encode(file_path, target)
@@ -52,13 +55,13 @@ else:
     if os.path.exists(f"{file_path_raw}.txt"):
         i += 1
 
-    while os.path.exists(f"{file_path_raw}.txt ({i})"):
+    while os.path.exists(f"{file_path_raw} ({i}).txt"):
         i += 1
 
     target = ""
 
     if i != 0:
-        target = f"{file_path_raw}.txt ({i})"
+        target = f"{file_path_raw} ({i}).txt"
 
     if algo == "h":
         huffman.decode(file_path, target)
